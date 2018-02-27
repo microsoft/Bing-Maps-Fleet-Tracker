c5e749c5b6a88c8dcbbdce46fed31606703e05fb
Mohamed Mohsen <momohs@microsoft.com>
2018-02-27 17:24:41 +0200

    Add the webhooks to the frontend

--- Frontend/src/app/shared/geofence.ts
+++ Frontend/src/app/shared/geofence.ts
@@ -4,6 +4,7 @@ export class Geofence {
     id: number;
     name: string;
     emailsToNotify: string[];
+    webhooksToNotify: string[];
     cooldown: number;
     fenceType: string;
     fencePolygon: Point[];
